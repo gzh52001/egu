d878899d4e81525e1ed6ee39ea99fbd083ab46c4
wuyule <859237215@qq,com>
2020-07-21 22:50:16 +0800

    cart

--- egu/src/page/Cart/bottom/index.js
+++ egu/src/page/Cart/bottom/index.js
@@ -1,28 +1,51 @@
 import React  from 'react'
 import {  Checkbox, Flex } from 'antd-mobile';
 import { Button } from 'antd';
+
 const CheckboxItem = Checkbox.CheckboxItem;
 const AgreeItem = Checkbox.AgreeItem;
-function Cartbottom() {
+let pitch=false ;
+class Cartbottom extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            isSelect: false
+        }
+    }
+    // console.log(props);
+   state={
+                yon:true,
+            }
+            // let pitch= props.data.cartData.every(item=>item.isSelect)
     
-    return(
-        <div className="bottom" style={{ width:"100%",height:"11.3vw",zIndex:"99",position: "fixed",bottom:"0",backgroundColor:'#fff'}}>
-        <Flex justify="between"> 
-               
-                <AgreeItem inline style={{color:'red'}} onChange={() => console.log('1')}>
-                    全选
-                </AgreeItem>
-                <div></div>
-                <div></div>
-                
-                <div className="van-submit-bar__text" style={{fontSize:"13.1px"}}><span>合计：</span><span className="van-submit-bar__price" style={{fontSize:"16.87px",color:"red"}}>¥ 0.00</span></div>
-               
-                <Button type="primary" style={{width:"24.5vw",height:"11.3vw"}} danger>
-                     去结算
-                </Button>   
-                
-            </Flex>
-        </div>
-    )
+    allSelect = () => {
+        this.props.allChange()
+    }
+           
+    render(){
+        let {yon,click} = this.state
+        // 控制全選
+        let { isAllSelect } = this.props;
+        return(
+            <div className="bottom" style={{ width:"100%",height:"11.3vw",zIndex:"99",position: "fixed",bottom:"0",backgroundColor:'#fff'}}>
+            <Flex justify="between"> 
+                   
+                    <AgreeItem inline style={{color:'red'}} checked={isAllSelect}
+                        onClick={this.allSelect}
+                        >
+                        全选
+                    </AgreeItem>    
+    
+                    
+                    <div className="van-submit-bar__text" style={{fontSize:"13.1px"}}><span>合计：</span><span className="van-submit-bar__price" style={{fontSize:"16.87px",color:"red"}}>¥ 0.00</span></div>
+                   
+                    <Button type="primary" style={{width:"24.5vw",height:"11.3vw"}} danger>
+                         去结算
+                    </Button>   
+                    
+                </Flex>
+            </div>
+        )
+    }
 }
 export default Cartbottom;
