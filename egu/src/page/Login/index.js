50e2e3e269410d920dc5b8b4f89f829facfb780c
wuyule <859237215@qq,com>
2020-07-22 14:16:19 +0800

    login

--- egu/src/page/Login/index.js
+++ egu/src/page/Login/index.js
@@ -8,14 +8,12 @@ import userApi from '@/api/user'
 
 import Pop from './../Bubble/bubble'
 import './style.scss'
-import { LeftOutlined } from '@ant-design/icons';
-import { Form, Input, Button } from 'antd';
-import Pop from '../Bubble/bubble' //引入气泡组件
 
 class Login extends Component {
 
-    jumpRout(rout) {
-        this.props.history.push(rout);
+    goReg = () => {
+        this.props.history.push('/register');
+
     }
 
     goBack = () => {
@@ -43,44 +41,58 @@ class Login extends Component {
             Toast.info('账号或密码错误')
         }
     }
+
+
     render(){
-        <div className='login'>
+        return (
+            <div className='login'>
+
                 {/* 头部 */}
                 <header>
-                    <div className='navLift' onClick={this.jumpRout.bind(this, '/mine')}><LeftOutlined style={{ fontSize: '20px', margin: '8px 0 0 8px' }} /></div>
+                    <div className='navLift' onClick={this.goBack}><LeftOutlined style={{ fontSize: '20px', margin: '8px 0 0 8px' }} /></div>
                     <div className='navTitle'>用户登录</div>
                     <div className='navRight'>
-                        {/* 使用气泡组件 */}
+                        {/* <EllipsisOutlined style={{ fontSize: '26px', margin: '8px 0 0 14px' }} /> */}
                         <Pop />
                     </div>
+
                 </header>
 
                 {/* 输入区 */}
                 <div className='login_box'>
-                    <Form className="login-form" validateMessages={verification}>
-                        <Form.Item name={['']} rules={[{ required: true }]}>
-                            <Input className='cardNumber' style={{ outline: 'medium' }} placeholder="请输入用户名/手机号/邮箱" />
+                    <Form className="login-form" onFinish = {this.onFinish}>
+                        <Form.Item
+                         name="username"
+                         rules={[{ required: true, message: '用户名不能为空' }]}
+                         style = {{height:44}}
+                        >
+                            <Input 
+                            className='cardNumber' 
+                            style={{ outline: 'medium' }} 
+                            placeholder="请输入用户名"  
+                            />
                         </Form.Item>
-                        <Form.Item name={['mima']} rules={[{ types: 'mima' }]}>
-                            <Input className='cardPass' type="password" placeholder="请输入6位以上的密码" />
+                        <Form.Item
+                         name="password"
+                         rules={[
+                            { required: true, message: '密码不能为空' },
+                            {max:10,min:6,message:'密码长度在6-15之间'}]}
+                         style = {{height:44}}
+                        >
+                            <Input.Password className='cardPass'  placeholder="请输入6位以上的密码" />
                         </Form.Item>
-                        {/*  <Form.Item className='verification'>
-                        <h4 style={{ fontSize: '9px', color: 'red' }}>* 请输入用户名/手机号/邮箱</h4>
-                    </Form.Item> */}
-                        <Form.Item>
+                        <div className="logname">
+                            <span className="b1" onClick={this.goReg}>点击注册</span>
+                        </div>
+                        <Form.Item style={{marginTop:9}}>
                             <Button type="primary" className="button" htmlType="submit">
                                 立即登录
-                        </Button>
+                            </Button>
                         </Form.Item>
                     </Form>
                 </div>
-                <div className="logname">
-                    <p className="b1" onClick={this.jumpRout.bind(this, '/register')}>手机快速注册</p>
-                    <p className="b2">忘记密码</p>
-                </div>
             </div>
+        )
     }
-       
-    }
-
-export default Login;
+}
+export default withRouter(Login);
