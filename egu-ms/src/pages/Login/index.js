19ed55dd05da57f6ed9e75a5f7a2cfa59d6dfb4a
wuyule <859237215@qq,com>
2020-07-23 20:21:41 +0800

    ms-login

--- egu-ms/src/pages/Login/index.js
+++ egu-ms/src/pages/Login/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Input, Space,Button  } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone,UserOutlined } from '@ant-design/icons';
 
+import "./css.scss"
 export default class Login extends Component {
+    loginToHome=()=>{
+
+    }
     render() {
         return (
-            <div>
-                Login
+            <div className="login">
+                <h1>依谷会员登录</h1>
+                <Space direction="vertical">
+                    <Input placeholder="username" prefix={<UserOutlined />} />
+                    <Input.Password
+                    placeholder="password"
+                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                    />
+                    <Button type="dashed" danger onClick={this.loginToHome}>login
+                    </Button>
+                         
+                 </Space>
             </div>
         )
     }
