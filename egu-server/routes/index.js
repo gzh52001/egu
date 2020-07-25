40daf31906b0461461f6949a48e1bb641d10031e
wuyule <859237215@qq,com>
2020-07-25 09:39:35 +0800

    '解决冲突'

--- egu-server/routes/index.js
+++ egu-server/routes/index.js
@@ -3,35 +3,25 @@ const router = express.Router();
 
 var bodyParser = require('body-parser');
 router.use(bodyParser.json()); // 转json数据
-router.use(bodyParser.urlencoded({extended:false})); // 转键值对数据 key=value&key=value
+router.use(bodyParser.urlencoded({
+    extended: false
+})); // 转键值对数据 key=value&key=value
 
 const categoryRouter = require('./modules/category');
 const goodsRouter = require('./modules/goods');
 const cartRouter = require('./modules/cart');
-const userRouter =  require('./modules/user');
-<<<<<<< HEAD
+const userRouter = require('./modules/user');
 const uploadRouter = require('./modules/upload');
-<<<<<<< HEAD
-=======
-const orderRouter =  require('./modules/order'); // 订单
->>>>>>> yuanyefeng
-=======
+const orderRouter = require('./modules/order'); // 订单
 const LoginRouter = require('./modules/msLogin');
->>>>>>> wuyule
 
 // use无论是什么类型的请求都可以进入
 router.use('/category', categoryRouter); // 开启category子路由
-router.use('/goods', goodsRouter); 
-router.use('/cart', cartRouter); 
-router.use('/user',userRouter);
-<<<<<<< HEAD
-router.use('/upload',uploadRouter);
-<<<<<<< HEAD
-=======
-router.use('/order',orderRouter);
->>>>>>> yuanyefeng
-=======
-router.use('/admin',LoginRouter);
->>>>>>> wuyule
+router.use('/goods', goodsRouter);
+router.use('/cart', cartRouter);
+router.use('/user', userRouter);
+router.use('/upload', uploadRouter);
+router.use('/order', orderRouter);
+router.use('/admin', LoginRouter);
 
 module.exports = router; // 导出
