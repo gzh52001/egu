3cff1580c97f13b72f522440b3eca33d7ddb2a8e
ename12 <2725402440@qq.com>
2020-07-25 09:05:27 +0800

    商品管理

---
+++ egu-ms/src/pages/Goods/data.js
@@ -0,0 +1,83 @@
+ /* ----------------行数据（后台数据）------------------- */
+ 
+ const dataSource= [
+  {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      business: '淘宝专营店',
+  },
+  {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      business: '京东特卖店',
+  },
+  {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      business: '2号店',
+  },
+  {
+      key: '4',
+      name: '衣服',
+      age: 50,
+      address: '广东省广州市天河区汇通产园',
+      business: '1号店'
+  },
+  {
+      key: '5',
+      name: '裤子',
+      age: 2,
+      address: '广西壮族自治区南宁市青乡区桂柳路',
+      business: '专卖店+'
+  },
+  {
+      key: '11',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      business: ['nice', 'developer'],
+  },
+  {
+      key: '21',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      business: ['loser'],
+  },
+  {
+      key: '31',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      business: ['cool', 'teacher'],
+  },
+  {
+      key: '41',
+      name: '衣服',
+      age: 50,
+      address: '广东省广州市天河区汇通产园',
+      business: '质量超好'
+  },
+  {
+      key: '51',
+      name: '裤子',
+      age: 2,
+      address: '广西壮族自治区南宁市青乡区桂柳路',
+      business: '广西产裤子就是耐穿'
+  },
+  {
+      key: '511',
+      name: '裤子',
+      age: 2,
+      address: '广西壮族自治区南宁市青乡区桂柳路',
+      business: '广西产裤子就是耐穿'
+  },
+]
+
+export default dataSource;
