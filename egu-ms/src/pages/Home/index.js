f3c560a54e8f9a4f9ae368318e305891d7645a10
poki-Q <577367252@qq.com>
2020-07-26 18:05:01 +0800

    test2

--- egu-ms/src/pages/Home/index.js
+++ egu-ms/src/pages/Home/index.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
  class Home extends Component {
     render() {
         return (
-            <div>
-                Home
+            <div style={{
+                    fontSize:40,fontWeight:600,
+                    marginLeft:50,marginTop:20
+                    }}>
+                欢迎使用依谷后台管理系统            
             </div>
         )
     }
