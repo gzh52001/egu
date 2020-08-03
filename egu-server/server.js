4d70be2129c86257e8a5e18b233070fd5442d3b8
poki-Q <577367252@qq.com>
2020-08-03 09:49:50 +0800

    后端-修改了多加了静态服务器的刷新找不到页面的代码

--- egu-server/server.js
+++ egu-server/server.js
@@ -6,13 +6,6 @@ const app = express();
 app.use(express.static('./'));//静态资源服务器
 app.use(allRouter); // 使用路由
 
-// 防止刷新找不到资源
-app.use((req, res) => {
-    const content = fs.readFileSync("./index.html");
-    res.set('Content-type','text/html;chartset=utf-8');
-    res.send(content.toString());
-})
-
 app.listen(8000, () => {
     console.log("开启服务成功，端口：8000");
 })
